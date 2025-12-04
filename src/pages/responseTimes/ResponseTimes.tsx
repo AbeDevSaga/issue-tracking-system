78db92bce9d87ba77ebd0f489fbab5ac61f4ce07
AbeDevSaga <2000amandanm@gmail.com>
2025-12-04 09:13:32 +0300

    response time added to base data

--- src/pages/responseTimes/ResponseTimes.tsx
+++ src/pages/responseTimes/ResponseTimes.tsx
@@ -1,6 +1,5 @@
 import IssueResponseTimeList from "../../components/tables/lists/responseTimesList";
 
-// import { useTranslation } from "react-i18next";
 export default function ResponseTimes() {
   // const { t } = useTranslation();
 
